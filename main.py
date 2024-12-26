a782eb3c1e4365abc6b4159daee54cb82e8e2a64
Salami-Taiwo-Oyekunle <salamitaiwo@yahoo.com>
2024-12-26 18:26:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+fname='suzie'
+lname='Salami'
+fullname=lname+fname
+print('Full name is',fullname)
+print(fullname*3)
+print('lenght of fullname is', len(fullname))
+print('third character', fullname[2])
+print('substring', fullname[2:6])
